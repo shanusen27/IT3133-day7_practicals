a3d7a42face4f3564594927d7d9fa557358aef45
shanusen27 <139607388+shanusen27@users.noreply.github.com>
2024-11-23 13:27:25 +0000

    passing Child component data to Parent component

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Childcom from './Childcom';
+import { useState } from 'react';
+
 
 function App() {
+  const [childname,setChildname]=useState("Default");
+  const receiveName=(name)=>{
+    setChildname(name);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello React I'm Parent</h2>
+      <Childcom setfun={receiveName}/>
+      <p>Parent:My child said:{childname}</p>
     </div>
   );
 }
