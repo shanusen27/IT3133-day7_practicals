4cb5c2ea66691e506f6db0314605984854ab8387
shanusen27 <139607388+shanusen27@users.noreply.github.com>
2024-11-23 13:37:19 +0000

    fixing error

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Childcom from './Childcom';
 import { useState } from 'react';
 
-
 function App() {
   const [childname,setChildname]=useState("Default");
   const receiveName=(name)=>{
